7b9755920ce4afaf1b724b8d486ee2550e5ade5f
Adao Jackson Batista Vidal De Negr <adao9167@digitalcollegesul.local>
2024-10-19 15:54:11 -0300

    nova API contendo outros itens

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-fetch("https://rickandmortyapi.com/api/character")
-.then(response => response.json())
-.then(data => {
-    for (let index = 0; index < data.results.length; index++) {
-        let corpo = document.querySelector(".tableBody")
-        let linha = document.createElement("tr")
-        linha.innerHTML = `
-            <td>${data.results[index].id}</td>
-            <td>${data.results[index].name}</td>
-            <td>${data.results[index].status}</td>
-            <td>${data.results[index].species}</td>
-            <td>${data.results[index].type}</td>
-            <td><img src="${data.results[index].image}" alt=""></td>
-            <td>${data.results[index].origin.name}</td>
-        `
-        corpo.appendChild(linha)
-    }
-})
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+// .then(response => response.json())
+// .then(data => {
+//     for (let index = 0; index < data.results.length; index++) {
+//         let corpo = document.querySelector(".tableBody")
+//         let linha = document.createElement("tr")
+//         linha.innerHTML = `
+//             <td>${data.results[index].id}</td>
+//             <td>${data.results[index].name}</td>
+//             <td>${data.results[index].status}</td>
+//             <td>${data.results[index].species}</td>
+//             <td>${data.results[index].type}</td>
+//             <td><img src="${data.results[index].image}" alt=""></td>
+//             <td>${data.results[index].origin.name}</td>
+//         `
+//         corpo.appendChild(linha)
+//     }
+// })
+
+
+
+
+// const buscarDados = async () => {
+//     const response = await fetch("https://gsi.fly.dev/characters")
+//     const data = await response.json()
+
+//     let corpo = document.querySelector(".tableBody")
+
+//     for (let index = 0; index < data.results.length; index++) {
+//         let linha = document.createElement("tr")
+//         linha.innerHTML=`
+//                 <td>${data.results[index].id}</td>
+//                 <td>${data.results[index].name}</td>
+//                 <td>${data.results[index].rarity}</td>
+//                 <td>${data.results[index].weapon}</td>
+//                 <td>${data.results[index].vision}</td>
+//         `
+//         corpo.appendChild(linha)
+//     }
+// }
+
+// buscarDados();
 
